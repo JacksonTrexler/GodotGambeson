0c12cdd38c421d31a60c265b6405bc46c97a2bdb
JacksonTrexler <jtrexler25@gmail.com>
2023-07-16 00:18:09 -0400

    Projectile collision

---
+++ scenes/ProjectileA.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class ProjectileA : RigidBody2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		string[] animTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
+		animatedSprite2D.Play("active");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	private void _on_visible_on_screen_notifier_2d_screen_exited()
+	{
+		QueueFree();
+		// Replace with function body.
+	}
+}
